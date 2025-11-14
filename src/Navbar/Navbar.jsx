dff66640365fcc940ddad860108dbc1fb23626e8
ridhukhan <prakritirshandhane@gmail.com>
2025-11-14 12:27:43 +0600

    update code

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './Navbar.css'
 import PrivecyPolicy from '../Pages/PrivecyPolicy'
 const Navbar = () => {
@@ -8,11 +8,11 @@ const Navbar = () => {
   return (
     <div className='NavBar'>
         <ul>
-            <li><Link to="/">Home</Link></li>
-             <li><Link to="/SmartWatch">Smart Watch</Link></li>
-              <li><Link to="/AirBards">Air Bards</Link></li>
-               <li><Link to="/Clothing">Clothing</Link></li>
-                <li><Link to="/Contact">Contact</Link></li>
+            <li><NavLink to="/" className="nav-menu">Home</NavLink></li>
+             <li><NavLink to="/SmartWatch" className="nav-menu">Smart Watch</NavLink></li>
+              <li><NavLink to="/AirBards" className="nav-menu">Air Bards</NavLink></li>
+               <li><NavLink to="/Clothing" className="nav-menu">Clothing</NavLink></li>
+                <li><NavLink to="/Contact" className="nav-menu">Contact</NavLink></li>
         </ul>
 
         <div className='dott' onClick={()=>setOpenmenu(!openmenu)}>
@@ -25,10 +25,10 @@ const Navbar = () => {
     <div className='menubar'>
       <ul>
         <li>
-          <Link to="/PrivecyPolicy">Privecy & Policy</Link><hr />
+          <NavLink to="/PrivecyPolicy">Privecy & Policy</NavLink><hr />
         </li>
         <li>
-          <Link to="/TermsCondition">Terms & Service</Link><hr />
+          <NavLink to="/TermsCondition">Terms & Service</NavLink><hr />
         </li>
 
       </ul>
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
-
+//import Navbar from '../Component/Navbar'
+//import Navbar from '../Component/Navbar'
+import fbpic from '../Assets/fb.png'
+import insta from '../Assets/instagram.png'
+import whatsapp from '../Assets/whatsapp.jpeg'
+import call from '../Assets/call.png'
+import './Contact.css'
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact pppp</h1>
-      <p>ghhfghdfgdfgxfgxxcxcxc</p>
-      <p>ghhfghdfgdfgxfgxxcxcxc</p>
-      <p>ghhfghdfgdfgxfgxxcxcxc</p>
-      <p>ghhfghdfgdfgxfgxxcxcxc</p>
-      <p>ghhfghdfgdfgxfgxxcxcxc</p>
-      <p>ghhfghdfgdfgxfgxxcxcxc</p>
+    <div  className='contactmain'>
+       <div ><h1 style={{color:"#fff666" ,backgroundColor:"#81580cc2",boxShadow:"2px 6px 10px #000", borderRadius:"10px"
 
+       }}>
+         If you Want <br/>
+      Connect with me?</h1>
+
+      <h3>Contact us</h3>
+    </div>
+     <div  className='contactus'>
+     <a href='https://www.facebook.com/riiiiiyad2/' target='_blank' rel='nopener noreferrer'> <img src={fbpic} alt='fb'/>facebook</a>
+   
+      <a href='https://www.instagram.com/ridhu_khan1/' target='_blank' rel='nopener noreferrer'> <img src={insta} alt='insta'/>instagram</a>
+      <a href='https://wa.me/8801836974525/' target='_blank' rel='nopener noreferrer'> <img src={whatsapp} alt='whatsapp'/>whatsapp</a>
+      <a href='tel:+8801742979691'> <img src={call} alt='call now'/>Call Me</a>
+
+    </div>
     </div>
   )
 }
